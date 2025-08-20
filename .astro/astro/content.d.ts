4338fdf75ff235ed7648912fb1a8525e5790c10d
chilrr <ccdg9907@gmail.com>
2025-08-21 01:07:19 +0800

    Updated content.d.ts

--- .astro/astro/content.d.ts
+++ .astro/astro/content.d.ts
@@ -141,9 +141,9 @@ declare module 'astro:content' {
 
 	type ContentEntryMap = {
 		"blog": {
-"2address.md": {
-	id: "2address.md";
-  slug: "2address";
+"address.md": {
+	id: "address.md";
+  slug: "address";
   body: string;
   collection: "blog";
   data: InferEntrySchema<"blog">
