dbb423c926f65634d71b7ff1dae90e6044c3ac1e
chilrr <ccdg9907@gmail.com>
2025-08-20 12:34:09 +0800

    Fix sharp module installation error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
-import imageTools from "astro-imagetools";
 
 export default defineConfig({
-    integrations: [imageTools()],
+    integrations: [],
 });
